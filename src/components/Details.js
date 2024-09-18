c4a76e372527f5cc5ce207c06f12df25a10158ad
kos4 <kos4.kks@gmail.com>
2024-09-18 22:23:15 +0500

    Список и детали

---
+++ src/components/Details.js
@@ -0,0 +1,44 @@
+import {useEffect, useState} from "react";
+
+export default function Details({info}) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const getUser = async () => {
+      try {
+        const res = await fetch(`https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${info.id}.json`);
+
+        if (!res.ok) throw new Error(res.statusText);
+
+        const data = await res.json();
+        setData(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (info) {
+      getUser(info.id);
+    }
+  }, [info]);
+
+  if (data) {
+    console.log(loading);
+    return (
+      <div className="user">
+        <img src={data.avatar} alt={data.name} className="user__photo"/>
+        <div className="user__name">{data.name}</div>
+        <div className="user__city">{data.details.city}</div>
+        <div className="user__company">{data.details.company}</div>
+        <div className="user__position">{data.details.position}</div>
+      </div>
+    );
+  } else {
+    return '';
+  }
+
+}
---
+++ src/components/List.js
@@ -0,0 +1,59 @@
+import {useEffect, useState} from "react";
+import Details from "./Details";
+
+export default function List() {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await fetch('https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json');
+
+        if (!res.ok) throw new Error(res.statusText);
+
+        const data = await res.json();
+        data.map(item => item.active = '');
+        setUsers(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getUsers();
+  }, []);
+
+  const handlerClick = event => {
+    const element = event.target;
+    const newUsers = [];
+    users.forEach(item => {
+      if (Number(element.id) === item.id) {
+        item.active = 'users__active';
+        setUser(item);
+      } else {
+        item.active = '';
+      }
+
+      newUsers.push(item);
+    });
+
+    setUsers(newUsers);
+  };
+
+  return (
+    <>
+      <div className="users">
+        {users.map(item => <User key={item.id} item={item} click={handlerClick}/>)}
+      </div>
+      <div className="user"></div>
+      <Details info={user}/>
+    </>
+  );
+}
+
+function User({item, click}) {
+  const className = `users__item ${item.active}`;
+  return (
+    <div className={className} onClick={click} id={item.id}>{item.name}</div>
+  );
+}
